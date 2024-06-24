f54bc7c5d7ecafb59d262e3d7fbbb0a4ea1845af
TuanPham0411 <eric.scorp93@gmail.com>
2024-06-25 01:37:12 +0700

    Add project files.

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,63 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using TaskManager.Models;
+
+namespace TaskManager.Controllers
+{
+    public class AccountController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Account/Register
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        // POST: Account/Register
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(User model)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Users.Add(model);
+                db.SaveChanges();
+                return RedirectToAction("Login");
+            }
+            return View(model);
+        }
+
+        // GET: Account/Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        // POST: Account/Login
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = db.Users.SingleOrDefault(u => u.Username == model.Username && u.Password == model.Password);
+                if (user != null)
+                {
+                    Session["UserId"] = user.Id;
+                    Session["Username"] = user.Username;
+                    return RedirectToAction("Index", "Home");
+                }
+                ModelState.AddModelError("", "Invalid username or password");
+            }
+            return View(model);
+        }
+
+        // GET: Account/Logout
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            return RedirectToAction("Login");
+        }
+    }
+}
---
+++ Models/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace TaskManager.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext() : base("DefaultConnection") { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
